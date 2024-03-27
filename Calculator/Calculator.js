0ff74a6eede6895c87a83b98815d0ad481b255f9
prajapatijigar <120376342+prajapatijigar@users.noreply.github.com>
2024-03-27 22:33:03 +0530

    assessment

---
+++ Calculator/Calculator.js
@@ -0,0 +1,29 @@
+let num = document.querySelectorAll(".number");
+let inpDisplay = document.getElementById("inp");
+let equalButton = document.querySelector(".equal");
+let operatorButtons = document.querySelectorAll(".operator");
+let deleteButton = document.querySelector(".delete");
+let inpBtnTwo = document.getElementById("inpTwo");
+
+console.log(inpBtnTwo);
+
+deleteButton.addEventListener("click", () => {
+  inpDisplay.value = inpDisplay.value.slice(0, -1);
+});
+
+equalButton.addEventListener("click", () => {
+  inpBtnTwo.value = eval(inpDisplay.value);
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    console.log(e.target.innerText);
+    inpDisplay.value += e.target.innerText;
+  });
+});
+
+num.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    inpDisplay.value += e.target.innerText;
+  });
+});
