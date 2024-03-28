854f5be56d729c68406bbb4c45dab27de8846cee
prajapatijigar <prajapatijigar2008@gmail.com>
2024-03-28 09:35:49 +0530

    Assessment

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -1,29 +1,38 @@
-let num = document.querySelectorAll(".number");
-let inpDisplay = document.getElementById("inp");
-let equalButton = document.querySelector(".equal");
-let operatorButtons = document.querySelectorAll(".operator");
-let deleteButton = document.querySelector(".delete");
-let inpBtnTwo = document.getElementById("inpTwo");
 
-console.log(inpBtnTwo);
-
-deleteButton.addEventListener("click", () => {
-  inpDisplay.value = inpDisplay.value.slice(0, -1);
-});
-
-equalButton.addEventListener("click", () => {
-  inpBtnTwo.value = eval(inpDisplay.value);
-});
-
-operatorButtons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    console.log(e.target.innerText);
-    inpDisplay.value += e.target.innerText;
+(function(){
+  let num = document.querySelectorAll(".number");
+  let inpDisplay = document.getElementById("inp");
+  let equalButton = document.querySelector(".equal");
+  let operatorButtons = document.querySelectorAll(".operator");
+  let deleteButton = document.querySelector(".delete");
+  let inpBtnTwo = document.getElementById("inpTwo");
+  
+  console.log(inpBtnTwo);
+  
+  deleteButton.addEventListener("click", () => {
+    inpDisplay.value = inpDisplay.value.slice(0, -1);
   });
-});
-
-num.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    inpDisplay.value += e.target.innerText;
+  
+  equalButton.addEventListener("click", () => {
+     try {
+      inpBtnTwo.value = eval(inpDisplay.value);
+      
+     } catch (error) {
+         inpDisplay.value = 'error'
+     }
   });
-});
+  
+  operatorButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      console.log(e.target.innerText);
+      inpDisplay.value += e.target.innerText;
+    });
+  });
+  
+  num.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      inpDisplay.value += e.target.innerText;
+    });
+  });
+  
+})()
